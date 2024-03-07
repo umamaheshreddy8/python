503d9273a62725d10801340193f4f0513f654157
umamaheshreddy8 <153646414+umamaheshreddy8@users.noreply.github.com>
2024-03-07 16:34:27 +0530

    Add files via upload

---
+++ Aloops1.py
@@ -0,0 +1,21 @@
+#
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+
+for x in range(1, 101):
+    if is_prime(x):
+        print(x)
+
+
+        
+            
+        
+            
+
+    
+    
